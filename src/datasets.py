6d2f958d8211c5a32a668c47098b9d59caf87eaf
DennisGankin <dennis.gankin@outlook.de>
2024-10-01 19:52:33 +0200

    dataset improvements

--- src/datasets.py
+++ src/datasets.py
@@ -134,8 +134,9 @@ class UKBSnpLevelDatasetH5(Dataset):
 
     def __getitem__(self, idx):
         # snp_id = self.snp_bed_ids[idx]
-        data = np.dstack(self.hdf_data[idx, :])
+        data = np.atleast_2d(self.hdf_data[idx, :])
         data = np.nan_to_num(data[:, self.snp_bed_ids])
+        data = np.dstack([data])
         data = torch.from_numpy(data).float()
 
         return data, self.labels[idx]
